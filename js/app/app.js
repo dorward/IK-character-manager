be38580e335c958a2006595a8a072b7fb91b9495
David Dorward <david@dorward.me.uk>
2013-10-18 22:06:52 +0100

    Able to search for an entry

--- js/app/app.js
+++ js/app/app.js
@@ -8,11 +8,25 @@ IK.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 
 IK.Router.map(function () {
-  this.resource('characters', { path: '/' });
+  //this.resource('characters', { path: '/' });
+  this.resource('characters', { path: "/" }, function () {
+  	this.resource('character', { path: "/:character_id"});
+  });
 });
 
 IK.CharactersRoute = Ember.Route.extend({
   model: function () {
+    return this.store.find('character');
+  }
+});
+
+IK.CharacterRoute = Ember.Route.extend({
+  model: function (params) {
+  	console.log("Character root", params, this);
+  	console.log("Store", this.store);
+  	console.log("Character", this.store.find('character', 2).then(function (character) {
+  		console.log(character.get('name'));
+  	}));
     return this.store.find('character');
   }
 });
