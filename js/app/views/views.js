6623be2bdb6b86d60be9908d9a9601e3bc9c3da4
David Dorward <david@dorward.me.uk>
2013-10-19 15:10:44 +0100

    Add XP to display

--- js/app/views/views.js
+++ js/app/views/views.js
@@ -14,3 +14,20 @@ IK.UnderTitleView = Ember.View.extend({
 	tagName: "div",
 	classNames: ["undertitle"]
 });
+
+IK.CharacterView = Ember.View.extend({
+	level: function () {
+		var xp = parseInt(this.controller.get('xp'), 10);
+		if (isNaN(xp)) {
+			return "";
+		}
+		if (xp < 50) {
+			return "Hero";
+		}
+		if (xp < 100) {
+			return "Veteren";
+		}
+		return "Epic";
+	}.property("xp")
+});
+
